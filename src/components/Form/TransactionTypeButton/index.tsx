a9388031b56c67335917f1db727fac78b70eb6d1
Anderson Lopes dos Santos <anderson_lops@hotmail.com>
2021-10-27 00:30:05 -0300

    Layout Formulario ok

---
+++ src/components/Form/TransactionTypeButton/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { TouchableOpacityProps } from "react-native";
+
+import { Container, Icon, Title } from "./styles";
+
+const icons = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+};
+interface IProps extends TouchableOpacityProps {
+  title: string;
+  type: "up" | "down";
+  isActive: boolean;
+}
+export function TransactionTypeButton({
+  title,
+  type,
+  isActive,
+  ...rest
+}: IProps) {
+  return (
+    <Container {...rest} isActive={isActive} type={type}>
+      <Icon type={type} name={icons[type]} />
+      <Title>{title}</Title>
+    </Container>
+  );
+}
---
+++ src/utils/categories.ts
@@ -0,0 +1,8 @@
+export const categories = [
+    { key: 'purchases', name: '0000 - PIX', icon: 'shopping-bag', color: '#5636D3' },
+    { key: 'food', name: '3391 - BRASIL', icon: 'coffee', color: '#FF872C' },
+    { key: 'salary', name: '0710 - SICREDI', icon: 'dollar-sign', color: '#12A454' },
+    { key: 'car', name: '6380 - BRADESCO', icon: 'crosshair', color: '#E83F5B' },
+    { key: 'leisure', name: '0001 - PAGSEGURO', icon: 'heart', color: '#26195C' },
+    { key: 'studies', name: '0002 - MERCADO PAGO', icon: 'book', color: '#9C001A' },
+]; 
